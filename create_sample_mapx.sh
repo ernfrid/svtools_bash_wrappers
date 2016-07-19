7484e347a7cbf0b1858a86ac95e558daa8d50a2d
Dave Larson <dlarson@genome.wustl.edu>
2016-07-19 12:27:31 -0500

    try to expand to absolute paths

--- create_sample_mapx.sh
+++ create_sample_mapx.sh
@@ -10,18 +10,27 @@ check_file_exists() {
     fi
 }
 
+# From http://stackoverflow.com/questions/17577093/how-do-i-get-the-absolute-directory-of-a-file-in-bash
+function abspath() {
+    (
+    cd $(dirname "$1")
+    DIR=$PWD/$(basename "$1")
+    echo $DIR
+    )
+}
+
 SAMPLE_MAP=$1
 LUMPY_DIR=$2
 
 #echo -e "Sample\tBAM\tSPLITTER"
 while read SAMPLE BAM
 do 
-    check_file_exists $BAM
+    check_file_exists $(abspath $BAM)
 
-    SPL=${BAM%.*}.splitters.bam
+    SPL=$(abspath ${BAM%.*}.splitters.bam)
     check_file_exists $SPL
 
-    HIST=$LUMPY_DIR/$SAMPLE/temp/cnvnator-temp/${BAM##*/}.hist.root
+    HIST=$(abspath $LUMPY_DIR/$SAMPLE/temp/cnvnator-temp/${BAM##*/}.hist.root)
     check_file_exists $HIST
 
     echo -e "$SAMPLE\t$BAM\t$SPL\t$HIST"
